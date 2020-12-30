593b4302d2770fb0e535ac6116f6ac86f5a5a396
Andrew Sterner <andrewjohnsterner@gmail.com>
2020-12-30 15:42:28 -0800

    AOC2020: day 19 pt2

--- 19_monsters/src/main/scala/Main.scala
+++ 19_monsters/src/main/scala/Main.scala
@@ -1,51 +1,13 @@
-import scala.util.matching.Regex
 import scala.io.Source
-import MonsterMessages.{parseMessages, matchesRule, toRegex}
+import MonsterMessages.countMatches
+
 object Main extends App {
-  val (rules, msgs) = parseMessages(Source.fromFile("input").getLines.toList)
-  val r0 = matchesRule(toRegex(rules(0))) _
-  println(msgs.count(r0))
+  val lines = Source.fromFile("input").getLines().toList
+  println(countMatches(lines, pt1 = true))
+  println(countMatches(lines, pt1 = false))
 }
 
 object MonsterMessages {
-  sealed abstract class Rule
-  case class Lit(c: Char) extends Rule
-  case class Chain(rs: List[Rule]) extends Rule
-  case class Or(r0: Rule, r1: Rule) extends Rule
-
-  def parseRules(lines: IterableOnce[String]): DefaultHashMap[Int, Rule] = {
-    val rawRules: Map[Int, String] = lines.map({case s"$k: $r" => (k.toInt, r)}).toMap
-    new DefaultHashMap[Int, Rule]((k, h) => {
-      def chain(s: String) = Chain(s.split(' ').toList.map(r => h(r.toInt)))
-      rawRules(k) match {
-        case s""""$c"""" => Lit(c.head)
-        case s"$r0 | $r1" => Or(chain(r0), chain(r1))
-        case rs => chain(rs)
-      }
-    })
-  }
-
-  def parseMessages(lines: List[String]) = {
-    lines.span(_.nonEmpty) match {
-      case (rs, _ :: ms) => (parseRules(rs), ms)
-      case _ => throw new Exception("no blank line")
-    }
-  }
-
-  def toRegexStr(r: Rule): String = r match {
-    case Lit(c) => c.toString()
-    case Chain(rs) => s"(?:${rs.foldLeft("")((a, r) => a ++ toRegexStr(r))})"
-    case Or(r0, r1) => s"(?:${toRegexStr(r0)}|${toRegexStr(r1)})"
-  }
-
-  def toRegex(r: Rule): Regex = raw"^${toRegexStr(r)}\z".r
-
-  def matchesRule(rule: Regex)(msg: String): Boolean = {
-    rule.matches(msg)
-  }
-}
-
-object MonsterMessages2 {
   sealed abstract class Rule
   case class Lit(c: Char) extends Rule
   case class Chain(rs: List[String]) extends Rule
@@ -60,27 +22,31 @@ object MonsterMessages2 {
     }
   }
 
-  def parseRules(lines: List[String]): Map[String, Rule] = lines.map(parseRule).toMap
+  def parseRules(lines: Iterable[String]) = lines.map(parseRule).toMap
 
-  def parseRulesPart2(lines: List[String]): Map[String, Rule] = {
+  def parseRulesPart2(lines: Iterable[String]) =
     parseRules(lines) + parseRule("8: 42 | 42 8") + parseRule("11: 42 31 | 42 11 31")
-  }
 
   def matchesRules(rules: Map[String, Rule])(msg: String) = {
-    def m(r: Rule, s: List[Char], succ: List[Char] => Boolean, fail: () => Boolean): Boolean = r match {
-      case Lit(c) => if (s.nonEmpty && s.head == c) succ(s.tail) else fail()
-      case Chain(r :: rs) => m(rules(r), s, s => m(Chain(rs), s, succ, fail), fail)
-      case Chain(Nil) => succ(s)
+    type Cont = () => Boolean
+    type Succ = (List[Char], Cont) => Boolean
+    def m(r: Rule, s: List[Char], succ: Succ, fail: Cont): Boolean = r match {
+      case Lit(c) => if (s.nonEmpty && s.head == c) succ(s.tail, fail) else fail()
+      case Chain(r :: rs) => m(rules(r), s, (s, f) => m(Chain(rs), s, succ, f), fail)
+      case Chain(Nil) => succ(s, fail)
       case Or(r0, r1) => m(r0, s, succ, () => m(r1, s, succ, fail))
     }
-    m(rules("0"), msg.toList, s => s.isEmpty, () => false)
+    m(rules("0"), msg.toList, (s, f) => s.isEmpty || f(), () => false)
   }
-}
 
-class DefaultHashMap[K, V](defaultFunc: (K, DefaultHashMap[K, V]) => V) extends scala.collection.mutable.HashMap[K, V] {
-  override def default(key: K): V = {
-    val value = defaultFunc(key, this)
-    put(key, value)
-    value
+  def splitOnEmpty(lines: List[String]) = lines.span(_.nonEmpty) match {
+      case (as, b :: bs) => (as, bs)
+      case (_, Nil) => throw new Exception("no empty line")
+  }
+
+  def countMatches(lines: List[String], pt1: Boolean = true): Int = {
+    val (rules, msgs) = splitOnEmpty(lines)
+    val rs = if (pt1) parseRules(rules) else parseRulesPart2(rules)
+    msgs.count(matchesRules(rs))
   }
 }
--- 19_monsters/src/test/scala/MsgSpec.scala
+++ 19_monsters/src/test/scala/MsgSpec.scala
@@ -11,28 +11,95 @@ class MsgSpec extends AnyFlatSpec with should.Matchers {
 3: 4 5 | 5 4
 4: "a"
 5: "b""""
-    val testRules = parseRules(testRulesStr.linesIterator)
-    val testRule0 = toRegex(testRules(0))
-    val matchRule0 = matchesRule(testRule0) _
+    val testRules = parseRules(testRulesStr.linesIterator.toList)
+    val matchesTestRules = matchesRules(testRules) _
 
-    "test rule 0" should "match ababbb" in {
-        assert(matchRule0("ababbb"))
+    "matchesTestRules" should "match ababbb" in {
+        assert(matchesTestRules("ababbb"))
     }
 
-    "test rule 0" should "not match bababa" in {
-        assert(!matchRule0("bababa"))
+    "matchesTestRules" should "not match bababa" in {
+        assert(!matchesTestRules("bababa"))
     }
 
-    "test rule 0" should "match abbbab" in {
-        assert(matchRule0("abbbab"))
+    "matchesTestRules" should "match abbbab" in {
+        assert(matchesTestRules("abbbab"))
     }
 
-    "test rule 0" should "not match aaabbb" in {
-        assert(!matchRule0("aaabbb"))
+    "matchesTestRules" should "not match aaabbb" in {
+        assert(!matchesTestRules("aaabbb"))
     }
 
-    "test rule 0" should "not match aaaabbb" in {
-        assert(!matchRule0("aaaabbb"))
+    "matchesTestRules" should "not match aaaabbb" in {
+        assert(!matchesTestRules("aaaabbb"))
     }
 
+    "matchTestRules pt2" should "match many 'a's" in {
+        val smallRecRulesStr = """0: 8
+42: "a"
+"""
+        val smallRecRules = parseRulesPart2(smallRecRulesStr.linesIterator.toList)
+        val m = matchesRules(smallRecRules) _
+        assert(m("a"))
+        assert(m("aa"))
+        assert(m("aaa"))
+        assert(m("a" * 100))
+
+    }
+
+    val recRulesTestStr = """42: 9 14 | 10 1
+9: 14 27 | 1 26
+10: 23 14 | 28 1
+1: "a"
+11: 42 31
+5: 1 14 | 15 1
+19: 14 1 | 14 14
+12: 24 14 | 19 1
+16: 15 1 | 14 14
+31: 14 17 | 1 13
+6: 14 14 | 1 14
+2: 1 24 | 14 4
+0: 8 11
+13: 14 3 | 1 12
+15: 1 | 14
+17: 14 2 | 1 7
+23: 25 1 | 22 14
+28: 16 1
+4: 1 1
+20: 14 14 | 1 15
+3: 5 14 | 16 1
+27: 1 6 | 14 18
+14: "b"
+21: 14 1 | 1 14
+25: 1 1 | 1 14
+22: 14 14
+8: 42
+26: 14 22 | 1 20
+18: 15 15
+7: 14 5 | 1 21
+24: 14 1
+
+abbbbbabbbaaaababbaabbbbabababbbabbbbbbabaaaa
+bbabbbbaabaabba
+babbbbaabbbbbabbbbbbaabaaabaaa
+aaabbbbbbaaaabaababaabababbabaaabbababababaaa
+bbbbbbbaaaabbbbaaabbabaaa
+bbbababbbbaaaaaaaabbababaaababaabab
+ababaaaaaabaaab
+ababaaaaabbbaba
+baabbaaaabbaaaababbaababb
+abbbbabbbbaaaababbbbbbaaaababb
+aaaaabbaabaaaaababaa
+aaaabbaaaabbaaa
+aaaabbaabbaaaaaaabbbabbbaaabbaabaaa
+babaaabbbaaabaababbaabababaaab
+aabbbbbaabbbaaaaaabbbbbababaaaaabbaaabba
+"""
+    "countMatches" should "count 3 matches" in {
+        countMatches(recRulesTestStr.linesIterator.toList) should be (3)
+    }
+    
+    "countMatches" should "count 12 matches after new rules" in {
+        countMatches(recRulesTestStr.linesIterator.toList, pt1 = false) should be (12)
+    }
 }
